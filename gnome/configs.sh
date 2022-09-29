234db8019ee04aac76a73245ab589a65987cf4cf
shasherazi <hassanrandomz@gmail.com>
2022-09-29 17:39:30 +0500

    show battery percentage

--- gnome/configs.sh
+++ gnome/configs.sh
@@ -7,6 +7,7 @@ gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']"
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 3700
+gsettings set org.gnome.desktop.interface show-battery-percentage true
 
 # plank
 dconf write /net/launchpad/plank/docks/dock1/theme "'mcOS-BS-iMacM1-Black'"
