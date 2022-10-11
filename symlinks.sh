616e4eb6d50bc48f8ebf883db20d5388b3be88a0
shasherazi <hassanrandomz@gmail.com>
2022-10-11 20:32:09 +0500

    added/removed some stuff, will organize later

--- symlinks.sh
+++ symlinks.sh
@@ -3,23 +3,13 @@ DOTFILES=~/dotfiles
 dirsToRemove=(
     .icons
     .themes
-    .spicetify
-    .zshrc
-    .p10k.zsh
-    .oh-my-zsh
     .local/share/plank
-    .local/share/ulauncher
 )
 
 dirsInDotfiles=(
     gnome/.icons
     gnome/.themes
-    spicetify/.spicetify
-    zsh/.zshrc
-    zsh/.p10k.zsh
-    zsh/.oh-my-zsh
     plank/plank
-    ulauncher/ulauncher
 )
 
 for index in ${!dirsToRemove[*]}; do
--- symlinksManualDeprecated.sh
+++ symlinksManualDeprecated.sh
@@ -6,20 +6,5 @@ ln -sf $DOTFILES/gnome/.icons ~/.icons
 rm -r ~/.themes
 ln -sf $DOTFILES/gnome/.themes ~/.themes
 
-rm -r ~/.spicetify
-ln -sf $DOTFILES/spicetify/.spicetify ~/.spicetify
-
-rm -r ~/.zshrc
-ln -sf $DOTFILES/zsh/.zshrc ~/.zshrc
-
-rm -r ~/.p10k.zsh
-ln -sf $DOTFILES/zsh/.p10k.zsh ~/.p10k.zsh
-
-rm -r ~/.oh-my-zsh
-ln -sf $DOTFILES/zsh/.oh-my-zsh ~/.oh-my-zsh
-
 rm -r ~/.local/share/plank
 ln -sf $DOTFILES/plank/plank ~/.local/share/plank
-
-rm -r ~/.local/share/ulauncher
-ln -sf $DOTFILES/ulauncher/ulauncher ~/.local/share/ulauncher
