663dce257e496a3f8e2c728618041b382bf77b7a
shasherazi <hassanrandomz@gmail.com>
2023-07-18 14:35:01 +0500

    time to reinstall

--- config/nvim/lua/shasherazi/keymaps.lua
+++ config/nvim/lua/shasherazi/keymaps.lua
@@ -65,17 +65,6 @@ keymap("t", "<C-\\>", "<CMD>lua require('FTerm').toggle()<CR>", opts)
 --[[ keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts) ]]
 --[[ keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts) ]]
 --[[ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts) ]]
--- telescope
-keymap("n", "<leader>f",
-  "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>",
-  opts)
-keymap("n", "<c-p>", "<cmd>Telescope live_grep<cr>", opts)
-
--- Nvimtree
-keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
-
--- formatting
-keymap("n", "<leader>=", ":Format<cr>", opts)
 
 -- ufo
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
--- config/nvim/lua/shasherazi/whichkey.lua
+++ config/nvim/lua/shasherazi/whichkey.lua
@@ -91,11 +91,14 @@ local mappings = {
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
   ["f"] = {
-    "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+    "<cmd>lua require('telescope.builtin').find_files()<cr>",
     "Find files",
   },
-  ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
+  ["F"] = {
+    "<cmd>lua require'telescope.builtin'.grep_string{ shorten_path = true, word_match = '-w', only_sort_text = true, search = '' }<cr>",
+    "Find Text" },
   ["P"] = { "<cmd>Telescope projects<cr>", "Projects" },
+  ["="] = { "<cmd>:Format<cr>", "Format" },
 
   p = {
     name = "Packer",
@@ -140,7 +143,7 @@ local mappings = {
       "<cmd>Telescope lsp_workspace_diagnostics<cr>",
       "Workspace Diagnostics",
     },
-    f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
+    f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format" },
     i = { "<cmd>LspInfo<cr>", "Info" },
     I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
     j = {
